3174e6cb14b58bfa6f75a4c6fd9ce0f2ae63a674
Easy Rider <115343312+mironovms@users.noreply.github.com>
2023-03-29 19:26:46 +0300

    Add files via upload
    1.5

--- selenium_internet101.py
+++ selenium_internet101.py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+from seleniumwire import webdriver
 import requests
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
@@ -22,47 +23,47 @@ def test_search_example():
 
     search_input.clear()
     search_input.send_keys(street)
-    time.sleep(1)
+    time.sleep(5)
     search_input.send_keys("\n")
-    time.sleep(1)
+    time.sleep(5)
 
     search_input = driver.find_element(By.XPATH,
                                        "//input[contains(@class, 'app141 app148 app147 app143 app160')]")
     search_input.clear()
     search_input.send_keys(dom)
-    time.sleep(1)
+    time.sleep(5)
     search_input.send_keys("\n")
-    time.sleep(1)
+    time.sleep(5)
 
     search_input = driver.find_element(By.XPATH, "/html/body/div/div/div[8]/div[1]/div/div/div/ul/li[1]")
-    time.sleep(1)
+    time.sleep(5)
     search_input.click()
-    time.sleep(1)
+    time.sleep(5)
 
     search_input = driver.find_element(By.XPATH,
                                        "//div[contains(@data-test, 'find_tohome_button')]")
-    time.sleep(1)
+    time.sleep(5)
     search_input.click()
-    time.sleep(1)
+    time.sleep(5)
 
     search_input = driver.find_element(By.XPATH, "/html/body/div/div/div[4]/div/div/div/div/div/span")
-    time.sleep(1)
+    time.sleep(5)
     search_input.click()
-    time.sleep(1)
+    time.sleep(5)
 
     search_input = driver.find_element(By.XPATH,
                                        "/html/body/div/div/div[1]/div[4]/div[4]/div[1]/div/div/div[2]/div[2]/div[6]/div/div/div[2]/div[2]/a")
-    time.sleep(1)
+    time.sleep(5)
     search_input.click()
-    time.sleep(1)
+    time.sleep(5)
 
     search_input = driver.find_element(By.XPATH,
                                        "//input[contains(@name, 'name')]")
     search_input.clear()
     search_input.send_keys(name)
-    time.sleep(1)
+    time.sleep(5)
     search_input.send_keys("\n")
-    time.sleep(1)
+    time.sleep(5)
 
     search_input = driver.find_element(By.XPATH,
                                        "//input[contains(@datatest, 'providers_provider_order_input_tel')]")
@@ -87,31 +88,38 @@ def test_search_example():
     #               "need_convergent": "false", "phone_connection": "71111111111", "region_id": "47", "street_id": "380434",
     #               "tariff_id": "102134003", "uuid": "c995eb05-d340-4b96-89b6-33de849d41d5"}
 
-    params = {
-      "fio": "Автотест",
-      "phone_connection": "71111111111",
-      "need_convergent": "false",
-      "convergent_tariff_id": 0,
-      "region_id": 47,
-      "lead_form_type": "{\"lead_form_type\":\"click_send_order_with_tariff\",\"userAgent\":\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36\",\"screenWidth\":1280,\"screenHeight\":1024,\"url\":\"https://piter-online.net/leningradskaya-oblast/orders/home?tariff_id=102134003\",\"from\":\"TC\"}",
-      "tariff_id": "102134003",
-      "uuid": "c995eb05-d340-4b96-89b6-33de849d41d5"
-    }
+    # params = {
+    #   "fio": "Автотест",
+    #   "phone_connection": "71111111111",
+    #   "need_convergent": "false",
+    #   "convergent_tariff_id": 0,
+    #   "region_id": 47,
+    #   "lead_form_type": "{\"lead_form_type\":\"click_send_order_with_tariff\",\"userAgent\":\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36\",\"screenWidth\":1280,\"screenHeight\":1024,\"url\":\"https://piter-online.net/leningradskaya-oblast/orders/home?tariff_id=102134003\",\"from\":\"TC\"}",
+    #   "tariff_id": "102134003",
+    #   "uuid": "c995eb05-d340-4b96-89b6-33de849d41d5"
+    # }
+    #
+    # headers = {'authority': 'orders.101internet.ru', "method": "POST", "path": "/api_external/sites/webhook?type=site101-order-home", "scheme": "https"}
+    #
+    # # forma = requests.get(driver.current_url)
+    # # forma = requests.get('https://piter-online.net/leningradskaya-oblast/rates?house_id=4270758')
+    #
+    # forma = requests.post('https://piter-online.net/leningradskaya-oblast/orders/home?tariff_id=105236004', headers=headers, data=params)
+    #
+    # print(forma.text)
+    # print(forma.status_code)
 
-    headers = {'authority': 'orders.101internet.ru', "method": "POST", "path": "/api_external/sites/webhook?type=site101-order-home", "scheme": "https"}
+    # for request in driver.requests[-1]:
+    #     if request.response:
+    #         print(driver.requests[-1]
+                # request.url,
+                # request.response.status_code,
+                # # request.response.headers['Content-Type']
+            # )
 
-    # forma = requests.get(driver.current_url)
-    # forma = requests.get('https://piter-online.net/leningradskaya-oblast/rates?house_id=4270758')
+    # status = driver.requests[-1].response.status_code
+    assert driver.requests[-1].response.status_code == 201
 
-    forma = requests.post('https://piter-online.net/leningradskaya-oblast/orders/home?tariff_id=105236004', headers=headers, data=params)
 
-    print(forma.text)
-    print(forma.status_code)
 
     driver.quit()
-
-
-
-
-
-
